ff44d118df467fc1dc94872969cad88990510034
Mansour Moufid <mansourmoufid@gmail.com>
2025-08-12 08:31:38 -0400

    Build archivers/xz.

--- build-packages.sh
+++ build-packages.sh
@@ -5,6 +5,11 @@ usr=$(pwd)/usr
 pkg=$(pwd)/pkg
 . $pkg/.pkgsrc_profile
 (
+    cd $usr/pkgsrc/archivers/xz
+    bmake configure || cat work/xz-*/config.log
+    bmake build install clean clean-depends
+)
+(
     cd $usr/pkgsrc/devel/gettext
     bmake build install clean clean-depends
 )
